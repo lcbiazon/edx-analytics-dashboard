160be89d17faa88c13e76073b350aeb1596649db
Tyler Hallada <thallada@edx.org>
2017-01-23 17:55:31 -0500

    Add header tooltips to the course-list table

--- analytics_dashboard/static/apps/course-list/list/views/base-header-cell.js
+++ analytics_dashboard/static/apps/course-list/list/views/base-header-cell.js
@@ -6,7 +6,16 @@ define(function(require) {
         CourseListBaseHeaderCell;
 
     CourseListBaseHeaderCell = BaseHeaderCell.extend({
-        container: '.course-list-table'
+        container: '.course-list-table',
+        tooltips: {
+            catalog_course_title: gettext('The title and course id of the course.'),
+            start_date: gettext('The date that the course starts on.'),
+            end_date: gettext('The date that the course ends on.'),
+            cumulative_count: gettext('Learners who ever enrolled in the course.'),
+            count: gettext('Learners currently enrolled in the course.'),
+            count_change_7_days: gettext('Net difference in current enrollment in the last week.'),
+            verified_enrollment: gettext('Number of currently enrolled learners pursuing a verified certificate of achievement.')
+        }
     });
 
     return CourseListBaseHeaderCell;
